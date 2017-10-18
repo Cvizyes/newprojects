8efe0c74f1708225f7e97336d4c13cff0323f08c
carlos <carlosvizcaino718@gmail.com>
2017-10-18 12:27:08 -0400

    this one is about age

--- script/script.js
+++ script/script.js
@@ -52,6 +52,7 @@ job = prompt('what does gabriel do?');
          console.log('something else');
  }
  */
+ /*
    var heighgabriel=185;
    var heighmarcos=145;
    var heighmartin=180;
@@ -62,6 +63,10 @@ job = prompt('what does gabriel do?');
    var scoremarcos = heighmarcos + 5 * agemarcos;
    var scoremartin= heighmartin+ 5* agemartin;
 
+
+   //the comparations//
+
+
    if ( scoremarcos > scoregabriel && scoremarcos > scoremartin){
    	console.log('marcos wins the game with' + scoremarcos + 'points');
    } else if ( scoremartin > scoremarcos && scoremartin > scoregabriel){
@@ -70,5 +75,21 @@ job = prompt('what does gabriel do?');
    console.log('gabriel wins the game with'+scoregabriel+'points')
    else if(scoremarcos === scoregabriel === scoremartin){
    	console.log('draw');
+   }*/
+
+   function calculateage(yearofbirth){
+   	var age = 2016 - yearofbirth;
+   	return age;
    }
 
+    var agegabriel = calculateage(1995);
+    var agemartin = calculateage(1987);
+    var agemarcos = calculateage(1990);
+   
+
+function yearsuntil(name,year) {
+var age = calculateage(year);
+var retirement = 45-age;
+console.log( name + " " + 'played in '+ retirement + 'years');
+}
+yearsuntil('gabriel',1995);
