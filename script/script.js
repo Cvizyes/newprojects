0ab39ee83d5a1c88816085161a2dd42aea0f6a9e
carlos <carlosvizcaino718@gmail.com>
2017-10-18 10:51:30 -0400

    i learned how to use cases

--- script/script.js
+++ script/script.js
@@ -10,19 +10,44 @@
 
 
 //////////////////////
-var name = 'carlos';
-var age = 22;
-var ismarried = 'yes';
+// var name = 'carlos';
+// var age = 22;
+// var ismarried = 'yes';
 
-if (ismarried === 'yes'){
-	console.log(name + ' is ' + " " + 'married');
-} else {
-	console.log ('will hopefully marry soon :)' );
-}
+// if (ismarried === 'yes'){
+// 	console.log(name + ' is ' + " " + 'married');
+// } else {
+// 	console.log ('will hopefully marry soon :)' );
+// }
  
- ismarried=false;
- if (ismarried){
- 	console.log('yes')
- }else {
-  console.log('no')
+//  ismarried=false;
+//  if (ismarried){
+//  	console.log('yes')
+//  }else {
+//   console.log('no')
+//  }
+
+
+ var age = 25;
+ if (age < 20 ){
+ 	console.log('gabriel is a teenager');
+ } else if(age >= 20 && age < 30){
+ 	console.log('gabriel is a young man')
+ }
+  else{
+	console.log('gabriel is a man')
+ }
+
+
+var job = 'driver';
+job = prompt('what does gabriel do?');
+ switch (job) {
+ 	case 'driver':
+ 	  console.log('gabriel drives a car');
+ 	  break;
+ 	 case 'teacher':
+         console.log ('gabriel teaches math');
+         break;
+         default:
+         console.log('something else');
  }
