521d92b020176ec5b294daf325fdf170240e798b
gabriel <carlosvizcaino718@gmail.com>
2017-10-17 23:13:47 -0400

    here i added a branch

--- script/script.js
+++ script/script.js
@@ -1,6 +1,12 @@
+//here i have already mi profile
 var name ='carlos';
 console.log(name);
 var lastname = 'vizcaino';
 console.log(lastname);
 var age = 22;
 console.log(age);
+
+// year
+var years = 22;
+var time = 2017 - years ;
+console.log (time);
