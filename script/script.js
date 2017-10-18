2e8afe391bc08505cfd28a9c6c45d4c454cec7f3
carlos <carlosvizcaino718@gmail.com>
2017-10-18 09:31:12 -0400

    I just created a new branch

--- script/script.js
+++ script/script.js
@@ -1,9 +1,28 @@
 //here i have already mi profile
-var name ='carlos';
-var lastname ='vizcaino' + " " +  name ;
-console.log(lastname);
+// var name ='carlos';
+// var lastname ='vizcaino' + " " +  name ;
+// console.log(lastname);
+// var age = 22;
+// console.log(age);
+// var years = 22;
+// var time = 2017 - years ;
+// console.log (time);
+
+
+//////////////////////
+var name = 'carlos';
 var age = 22;
-console.log(age);
-var years = 22;
-var time = 2017 - years ;
-console.log (time);
+var ismarried = 'yes';
+
+if (ismarried === 'yes'){
+	console.log(name + ' is ' + " " + 'married');
+} else {
+	console.log ('will hopefully marry soon :)' );
+}
+ 
+ ismarried=false;
+ if (ismarried){
+ 	console.log('yes')
+ }else {
+  console.log('no')
+ }
