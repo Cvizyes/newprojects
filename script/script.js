000759dae0f76bd84ef7152b6dced1a9ae0d8c1d
carlos <carlosvizcaino718@gmail.com>
2017-10-18 00:05:23 -0400

    other

--- script/script.js
+++ script/script.js
@@ -1,12 +1,9 @@
 //here i have already mi profile
 var name ='carlos';
-console.log(name);
-var lastname = 'vizcaino';
+var lastname ='vizcaino' + " " +  name ;
 console.log(lastname);
 var age = 22;
 console.log(age);
-
-// year
 var years = 22;
 var time = 2017 - years ;
 console.log (time);
